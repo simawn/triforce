fa3e1aa5162fe9bb081f2a980127c49fbf52cfd8
Simon <simon@simonhuang.ca>
2017-01-28 22:18:27 -0500

    twitter implementation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var masterSearch = require('../masterSearch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test/:id', function(req, res, next){
+  masterSearch.showResult(req.params.id, function(results){
+      res.render('test', {output: req.params.id,
+        thisresult: results
+      });
+
+  });
+});
+
+router.post('/test/submit', function(req, res, next){
+  var id = req.body.id;
+  res.redirect('/test/' + id);
 });
 
 module.exports = router;
